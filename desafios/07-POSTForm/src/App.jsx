43c9435c3685356a86ad37e963f02ab5e6fd3b36
Willian Oliveira <williancontato67@gmail.com>
2024-01-22 20:51:06 -0300

    Organização do eslintrc

--- desafios/07-POSTForm/src/App.jsx
+++ desafios/07-POSTForm/src/App.jsx
@@ -14,55 +14,71 @@
 
 // Mostre uma mensagem na tela, caso a resposta da API seja positiva
 
-import React from "react"
-import Input from "./Input";
-
+import React from 'react';
+import Input from './Input';
 
 function App() {
   const [form, setForm] = React.useState({
-    nome: "",
-    email: "",
-    senha: "",
-    cep: "",
-    rua: "",
-    numero: "",
-    bairro: "",
-    cidade: "",
-    estado: "",
-  })
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
 
-  const [response, setResponse] = React.useState("")
+  const [response, setResponse] = React.useState('');
 
   function enviarForm(event) {
-    event.preventDefault()
+    event.preventDefault();
     fetch('https://ranekapi.origamid.dev/json/api/usuario', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(form),
-    }).then(response => {
-      setResponse(response)
-    })
+    }).then((response) => {
+      setResponse(response);
+    });
   }
 
   return (
     <>
       <form onSubmit={enviarForm}>
-        <Input nome="Nome" id="nome" form={form} setForm={setForm}/>
-        <Input nome="Email" id="email" form={form} setForm={setForm}/>
-        <Input nome="Senha" id="senha" tipo="password" form={form} setForm={setForm}/>
-        <Input nome="Cep" id="cep" tipo="number" form={form} setForm={setForm}/>
-        <Input nome="Rua" id="rua" form={form} setForm={setForm}/>
-        <Input nome="Numero" id="numero"tipo="number" form={form} setForm={setForm}/>
-        <Input nome="Bairro" id="bairro" form={form} setForm={setForm}/>
-        <Input nome="Cidade" id="cidade" form={form} setForm={setForm}/>
-        <Input nome="Estado" id="estado" form={form} setForm={setForm}/>
-        <button style={{marginTop: "20px"}} >Enviar</button>
+        <Input nome="Nome" id="nome" form={form} setForm={setForm} />
+        <Input nome="Email" id="email" form={form} setForm={setForm} />
+        <Input
+          nome="Senha"
+          id="senha"
+          tipo="password"
+          form={form}
+          setForm={setForm}
+        />
+        <Input
+          nome="Cep"
+          id="cep"
+          tipo="number"
+          form={form}
+          setForm={setForm}
+        />
+        <Input nome="Rua" id="rua" form={form} setForm={setForm} />
+        <Input
+          nome="Numero"
+          id="numero"
+          tipo="number"
+          form={form}
+          setForm={setForm}
+        />
+        <Input nome="Bairro" id="bairro" form={form} setForm={setForm} />
+        <Input nome="Cidade" id="cidade" form={form} setForm={setForm} />
+        <Input nome="Estado" id="estado" form={form} setForm={setForm} />
+        <button style={{ marginTop: '20px' }}>Enviar</button>
       </form>
       {response && response.ok && <p>Usuário Criado</p>}
     </>
-
   );
 }
 
--- desafios/07-POSTForm/src/Input.jsx
+++ desafios/07-POSTForm/src/Input.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Input = ({ nome, tipo="text", id, form ,setForm}) => {
+const Input = ({ nome, tipo = 'text', id, form, setForm }) => {
   function colocarDados({ target }) {
-    const {id, value} = target
-    setForm({...form, [id]: value})
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
   }
-
 
   return (
     <>
       <label htmlFor={id}>{nome}</label>
-      <input type={tipo} placeholder={nome} id={id} onChange={colocarDados}/>
+      <input type={tipo} placeholder={nome} id={id} onChange={colocarDados} />
     </>
+  );
+};
 
-    
-  )
-}
-
-export default Input
+export default Input;
