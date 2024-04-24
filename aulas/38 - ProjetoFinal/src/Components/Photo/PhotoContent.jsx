8d127458d171aa091fff372f222c4f805491c09a
Willian Oliveira <williancontato67@gmail.com>
2024-04-24 13:23:21 -0300

    Finalização da dos commentários e o botão de deletar uma imagem

--- aulas/38 - ProjetoFinal/src/Components/Photo/PhotoContent.jsx
+++ aulas/38 - ProjetoFinal/src/Components/Photo/PhotoContent.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
+  const user = React.useContext(UserContext);
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -11,10 +16,14 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.vizualizacoes}>{photo.acessos}</span>
           </p>
-          <span className={styles.vizualizacoes}>{photo.acessos}</span>
           <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
@@ -26,6 +35,7 @@ const PhotoContent = ({ data }) => {
           </ul>
         </div>
       </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
